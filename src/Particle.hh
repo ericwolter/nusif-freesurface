b58fcda525321af183c75126ab0676e3a9f79531
Mohammad Farahani <mh.najafabadi@gmail.com>
2014-01-20 18:44:13 +0100

    Particle.hh is added

---
+++ src/Particle.hh
@@ -0,0 +1,40 @@
+#ifndef PARTICLE_HH
+#define PARTICLE_HH
+
+#include <iostream>
+#include "Types.hh"
+
+
+//*******************************************************************************************************************
+/*    Particl class for 2 dimensions
+*
+*    
+*/
+//*******************************************************************************************************************
+
+
+class Particle{
+
+public:
+
+   real & x()    { return x_;    }
+   real & y()    { return y_;    }
+   real & u()    { return u_;    }
+   real & v()    { return v_;    }
+
+   const real & x() const { return x_;    }
+   const real & y() const { return y_;    }
+   const real & u() const { return u_;    }
+   const real & v() const { return v_;    }
+   //inline real & operator () ( int i ,int j );
+   //const  real & operator () ( int i ,int j ) const;	
+
+private:
+
+  real x_ ;   // "x" position of particle
+  real y_ ;   // "y" position of particle
+  real u_ ;   // "u" position of particle
+  real v_ ;   // "v" position of particle
+}
+
+#endif PARTICLE_HHH

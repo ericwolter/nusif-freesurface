533c47e448b100f7ba6b57b6c7a52984f4e8fb99
Eric Wolter <eric.wolter@gmx.de>
2014-01-22 13:13:26 +0100

    added Particle class

--- src/Particle.hh
+++ src/Particle.hh
@@ -8,33 +8,62 @@
 //*******************************************************************************************************************
 /*    Particl class for 2 dimensions
 *
-*    
+*
 */
 //*******************************************************************************************************************
 
 
-class Particle{
+class Particle
+{
 
 public:
 
-   real & x()    { return x_;    }
-   real & y()    { return y_;    }
-   real & u()    { return u_;    }
-   real & v()    { return v_;    }
+    real &x()
+    {
+        return x_;
+    }
+    real &y()
+    {
+        return y_;
+    }
+    real &u()
+    {
+        return u_;
+    }
+    real &v()
+    {
+        return v_;
+    }
 
-   const real & x() const { return x_;    }
-   const real & y() const { return y_;    }
-   const real & u() const { return u_;    }
-   const real & v() const { return v_;    }
-   //inline real & operator () ( int i ,int j );
-   //const  real & operator () ( int i ,int j ) const;	
+    const real &x() const
+    {
+        return x_;
+    }
+    const real &y() const
+    {
+        return y_;
+    }
+    const real &u() const
+    {
+        return u_;
+    }
+    const real &v() const
+    {
+        return v_;
+    }
+    //inline real & operator () ( int i ,int j );
+    //const  real & operator () ( int i ,int j ) const;
+
+    int getCellX(real dx);
+    int getCellY(real dy);
 
 private:
 
-  real x_ ;   // "x" position of particle
-  real y_ ;   // "y" position of particle
-  real u_ ;   // "u" position of particle
-  real v_ ;   // "v" position of particle
-}
+    real x_ ;   // "x" position of particle
+    real y_ ;   // "y" position of particle
+    real u_ ;   // "u" velocity of particle
+    real v_ ;   // "v" velocity of particle
 
-#endif PARTICLE_HHH
+};
+
+#endif //PARTICLE_HHH
